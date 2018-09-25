534276a8de9905075ec947d4f9f48d2fca55552e
PJ Pollina <contact@pjpollina.info>
2018-09-24 20:16:01 -0400

    Added support for paths using '~/' shortcut

--- src/io.c
+++ src/io.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "defs.h"
+
+static char* path_expansion(char* filepath) {
+  if(filepath[0] == '~' && filepath[1] == '/') {
+    char* buffer = malloc(4096);
+    strcpy(buffer, getenv("HOME"));
+    strcat(buffer, &filepath[1]);
+    return buffer;
+  }
+  return filepath;
+}
 
 int write_listfile(struct tasklist* tl, char* filepath) {
   // open file
   FILE* outfile;
-  outfile = fopen(filepath, "w");
+  outfile = fopen(path_expansion(filepath), "w");
   if(!outfile) {
     fprintf(stderr, "Error opening file.\n");
     return EXIT_FAILURE;
@@ -24,7 +35,7 @@ int write_listfile(struct tasklist* tl, char* filepath) {
 int read_listfile(struct tasklist* tl, char* filepath) {
   // open file
   FILE* infile;
-  infile = fopen(filepath, "r");
+  infile = fopen(path_expansion(filepath), "r");
   if(!infile) {
     fprintf(stderr, "Error opening file.\n");
     return EXIT_FAILURE;
